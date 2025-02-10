bc07560bae589006bc1ca31caeae188b7cc0019d
FranXSAM <genanimeofc27@outlook.com>
2025-02-10 16:16:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '..'
+def home():
+    return "Flask App Running!"
 
+@app.route('/health')
+def health_check():
+    return "OK", 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
-  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
